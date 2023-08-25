5f79ccc394b91b8898da3bd68cd6bef79d3d4632
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-08-25 19:15:35 +0430

    Refactor code and using API

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,27 +1,36 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import BookCard from './bookCard';
 import AddBook from './addBook';
+import { fetchBook } from '../redux/books/booksSlice';
 
 export default function Book() {
-  const data = useSelector((state) => state.book);
+  const { books } = useSelector((state) => state.books);
+  const { isLoading } = useSelector((state) => state.books.isLoading);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBook());
+  }, [books, dispatch]);
+
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <>
       <ul>
-        {
-          data.map((article) => (
-            <li key={article.item_id}>
-              <BookCard
-                id={article.item_id}
-                category={article.category}
-                title={article.title}
-                author={article.author}
-                chapter={article.chapter}
-                percentValue={article.percentValue}
-              />
-            </li>
-          ))
-        }
+        {books.map((book) => (
+          <li key={book.item_id}>
+            <BookCard
+              id={book.item_id}
+              category={book.category}
+              title={book.title}
+              author={book.author}
+              chapter="Introduction"
+              percentValue={0}
+            />
+          </li>
+        ))}
       </ul>
       <hr />
       <AddBook />
